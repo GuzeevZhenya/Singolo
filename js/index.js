0a37fd8d56e1f18d05e132d524d84b0c06809c9b
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-12-08 15:33:08 +0300

    add

--- js/index.js
+++ js/index.js
@@ -18,13 +18,13 @@
          button.addEventListener('click', () => {
              const currentCategory = button.dataset.filter;
              filter(currentCategory, cards);
-         })
-     })
+         });
+     });
  }
 
  app();
 
-
+ //Popap
  function popap() {
      const menuButton = document.querySelector('.header__button');
      const menuList = document.querySelector('.header__menu');
@@ -36,8 +36,51 @@
          menuButton.setAttribute('aria-expanded', !expanded);
          menuButton.classList.toggle('header__button--open');
          menuList.classList.toggle('header__menu--open');
-
      });
  }
 
  popap();
+
+
+ //Slider
+ let images = document.querySelectorAll('.infinity-slider .slider-div');
+ let current = 0;
+ let color = document.querySelector('.slider');
+ let secondSlider = document.querySelector('.slider-div--second');
+
+ function slider() {
+     for (let i = 0; i < images.length; i++) {
+         images[i].classList.add('opacity0');
+     }
+     images[current].classList.remove('opacity0');
+ }
+
+ document.querySelector('.btn-infinity-up').onclick = function() {
+     if (current - 1 == -1) {
+         color.style.background = '#648BF0';
+         color.style.borderColor = '#154bda';
+         secondSlider.style.opacity = 1;
+         current = images.length - 1;
+     } else {
+         color.style.background = '#f06c64';
+         color.style.borderColor = '#ea676b';
+         secondSlider.style.opacity = 0;
+         current--;
+     }
+     slider();
+ }
+
+ document.querySelector('.btn-infinity-down').onclick = function() {
+     if (current + 1 == images.length) {
+         color.style.background = '#f06c64';
+         color.style.borderColor = '#ea676b';
+         secondSlider.style.opacity = 0;
+         current = 0;
+     } else {
+         color.style.background = '#648BF0';
+         color.style.borderColor = '#154bda';
+         secondSlider.style.opacity = 1;
+         current++;
+     }
+     slider();
+ };
