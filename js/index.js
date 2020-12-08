1f944a743645915d895e89ae27dd19348c4116b1
GuzeevZhenya <shyst.evgeny@mail.ru>
2020-12-08 11:46:52 +0300

    add

--- js/index.js
+++ js/index.js
@@ -1,27 +1,43 @@
-function app() {
-    const buttons = document.querySelectorAll('.portfolio__tag');
-    const cards = document.querySelectorAll('.card');
+ function app() {
+     const buttons = document.querySelectorAll('.portfolio__tag');
+     const cards = document.querySelectorAll('.card');
+
+     function filter(category, items) {
+         items.forEach(item => {
+             const isItemFiltered = !item.classList.contains(category);
+             const isShowAll = category.toLowerCase() === 'all';
+             if (isItemFiltered && !isShowAll) {
+                 item.classList.add('hide');
+             } else {
+                 item.classList.remove('hide');
+             }
+         });
+     }
+
+     buttons.forEach(button => {
+         button.addEventListener('click', () => {
+             const currentCategory = button.dataset.filter;
+             filter(currentCategory, cards);
+         })
+     })
+ }
+
+ app();
 
 
-    function filter(category, items) {
-        items.forEach((item) => {
-            const isItemFiltered = !item.classList.contains(category);
-            const isShowAll = category.toLowerCase() === 'all'
-            if (isItemFiltered && !isShowAll) {
-                item.classList.add('hide');
-            } else {
-                item.classList.remove('hide');
-            }
-        });
-    }
+ function popap() {
+     const menuButton = document.querySelector('.header__button');
+     const menuList = document.querySelector('.header__menu');
 
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            const currentCategory = button.dataset.filter;
-            filter(currentCategory, cards);
-            console.log(currentCategory);
-        })
-    })
-}
+     //Добавляем обработчик событик к menuButton
+     menuButton.addEventListener('click', () => {
+         let expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+         //После нажатия меняем значение атрибута
+         menuButton.setAttribute('aria-expanded', !expanded);
+         menuButton.classList.toggle('header__button--open');
+         menuList.classList.toggle('header__menu--open');
 
-app();
+     });
+ }
+
+ popap();
