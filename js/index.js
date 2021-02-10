39874b6a899c931394c1cdf75bf2da55dcda5538
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-02-10 15:19:00 +0300

    refactoring burger,add new login in slider

--- js/index.js
+++ js/index.js
@@ -34,20 +34,7 @@ function app() {
     })
 }
 
-
 app();
-
-// //Скрывает картинки, если их больше 12
-// function photoHidden() {
-//     let images = document.querySelectorAll(".portfolio__images img");
-//     for (let i = 0; i < images.length; i++) {
-//         if (i >= 12) {
-//             images[i].style.display = "none";
-//         }
-//     }
-// }
-
-// photoHidden();
 
 function headerSelecter() {
     const menuList = document.querySelector('.header__menu');
@@ -57,7 +44,8 @@ function headerSelecter() {
             if (active) {
                 active.classList.remove('header__menu-link--selected');
             }
-            e.target.classList.add('header__menu-link--selected');
+            menuList.classList.toggle('header__menu--open');
+
         }
     })
 }
@@ -67,6 +55,7 @@ headerSelecter();
 
 //Popap
 function popap() {
+    const openMenuButton = document.querySelector('.header__button ');
     const menuButton = document.querySelector('.header__button');
     const menuList = document.querySelector('.header__menu');
 
@@ -82,68 +71,11 @@ function popap() {
 
 popap();
 
-//  let position = 0;
-//  const slidesToShow = 1;
-//  const slidesToScroll = 1;
-//  const container = document.querySelector('.slider__inner');
-//  const track = document.querySelector('.slider-track');
-//  const btnPrev = document.querySelector('.slider__left');
-//  const btnNext = document.querySelector('.slider__right');
-//  const items = document.querySelectorAll('.slider-div');
-//  const itemsCount = items.length;
-//  const itemWidth = container.clientWidth / slidesToShow;
-//  const movePosition = slidesToScroll * itemWidth;
-
-//  items.forEach((item) => {
-//      item.style.minWidth = `${itemWidth}px`;
-//  });
-
-//  btnNext.addEventListener('click', () => {
-//      const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth) / itemWidth;
-//      position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-//      setPosition();
-//      checkBtns();
-//  });
-
-//  btnPrev.addEventListener('click', () => {
-//      const itemsLeft = Math.abs(position) / itemWidth;
-//      position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth;
-//      setPosition();
-//      checkBtns();
-//  });
-
-//  const setPosition = () => {
-//      track.style.transform = `translateX(${position}px)`;
-//  };
-
-//  const checkBtns = () => {
-//      btnPrev.disabled = position === 0;
-//      btnNext.disabled = position <= -(itemsCount - slidesToShow) * itemWidth;
-//  };
-
-//  checkBtns();
-
-
-//  function scrolling() {
-//      const anchors = document.querySelectorAll("a[href*='#']");
-//      for (let anchor of anchors) {
-//          anchor.addEventListener('click', (e) => {
-//              e.preventDefault();
-//              const blockID = anchor.getAttribute('href');
-//              document.querySelector('' + blockID).scrollIntoView({
-//                  behavior: "smooth",
-//                  block: "start"
-//              })
-//          })
-//      }
-//  }
-
-//  scrolling();
-
 const imgs = document.getElementById('imgs');
 const img = document.querySelectorAll('#imgs .slider__image');
 const buttonLeft = document.querySelector(".slider__left");
 const buttonRight = document.querySelector(".slider__right");
+const screenWidth = window.screen.width;
 let idx = 0;
 
 
@@ -156,7 +88,11 @@ function leftClick() {
         idx = img.length - 1;
     }
 
-    imgs.style.transform = `translateX(${-idx * 1020}px)`;
+    if (screenWidth > 1020) {
+        imgs.style.transform = `translateX(${-idx* 1020 }px)`;
+    } else {
+        imgs.style.transform = `translateX(${-idx* screenWidth}px)`;
+    }
 }
 
 function rightClick() {
@@ -164,7 +100,12 @@ function rightClick() {
     if (idx > img.length - 1) {
         idx = 0;
     }
-    imgs.style.transform = `translateX(${-idx*1020}px)`;
+    if (screenWidth > 1020) {
+        imgs.style.transform = `translateX(${-idx* 1020 }px)`;
+    } else {
+        imgs.style.transform = `translateX(${-idx* screenWidth }px)`;
+    }
+
 }
 
 //Прокрутка хедера 
